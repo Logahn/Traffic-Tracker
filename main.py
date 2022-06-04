485320f9eb24dddc49a9ec28e996b1f96a0d1654
Adele Chinda <chindahadele2@gmail.com>
2022-06-04 14:50:37 +0300

    Car detection implemented

--- main.py
+++ main.py
@@ -2,39 +2,45 @@
 import cv2
 import  random
 
-# Training algo for vehicles
-#vehicle_classifier_file = 
-vehicle_classifier = cv2.CascadeClassifier("vehicle_classifier_file")
+#* Training algo for cars
+cars_classifier_file = "modules/cars.xml"
+car_classifier = cv2.CascadeClassifier("modules/cars.xml")
 
-# Training algo for pedestraints
+#* Training algo for bikes
+bike_classifier_file = "modules/bike.xml"
+bike_classifier = cv2.CascadeClassifier("bike_classifier_file")
+
+#* Training algo for buses
+bus_classifier_file = "modules/bus.xml"
+bus_classifier = cv2.CascadeClassifier("bus_classifier_file")
+
+#* Training algo for pedestraints
+pedestrians_classifier_file = "modules/pedestrians.xml"
+pedestrians_classifier = cv2.CascadeClassifier("pedestrians_classifier_file")
+
+font = cv2.FONT_HERSHEY_PLAIN
 
 
-
-
-
-
-# font = cv2.FONT_HERSHEY_PLAIN
-# trained_faces_data = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-# trained_palm_data = cv2.CascadeClassifier("palm.xml")
-
-# # #* Choose image to detect
-# # img = cv2.imread("images/multiple.png")
+#* Choose image to detect
+img = cv2.imread("data/data1.jpg")
 
 # # #* Convert the data to greyscale
-# # greyscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+greyscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # # #* Detect faces
-# # face_coordinates = trained_faces_data.detectMultiScale(greyscaled_img)
+face_coordinates = car_classifier.detectMultiScale(greyscaled_img)
 
 # # #* Draw rectangles around faces
-# # colors = ((256,0,0), (0,256,0), (0,0,256))
+colors = ((256,0,0), (0,256,0), (0,0,256))
 
-# # for (x, y, w, h) in face_coordinates:
-# #     color = random.choice(colors)
-# #     cv2.rectangle(img, (x, y),(x+w, y+h), color, 2)
+for (x, y, w, h) in face_coordinates:
+    color = random.choice(colors)
+    cv2.rectangle(img, (x, y),(x+w, y+h), color, 2)
+    cv2.putText(img, str("Cars"), (x+5,y-5), font,1,color,1)
 
-# # cv2.imshow("Test data", img)
-# # cv2.waitKey()
+cv2.imshow("Test data1", img)
+
+cv2.waitKey()
 
 
 # #* Capture video from webcam
