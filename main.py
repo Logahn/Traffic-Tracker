4c5962584cb8c5562c4a19a5ea1e2dc9196d8c90
Adele Chinda <chindahadele2@gmail.com>
2022-06-04 21:58:13 +0300

    Modules Modification

--- main.py
+++ main.py
@@ -3,44 +3,62 @@ import cv2
 import  random
 
 #* Training algo for cars
-cars_classifier_file = "modules/cars.xml"
 car_classifier = cv2.CascadeClassifier("modules/cars.xml")
 
 #* Training algo for bikes
-bike_classifier_file = "modules/bike.xml"
-bike_classifier = cv2.CascadeClassifier("bike_classifier_file")
+bike_classifier = cv2.CascadeClassifier("modules/bike.xml")
 
 #* Training algo for buses
-bus_classifier_file = "modules/bus.xml"
-bus_classifier = cv2.CascadeClassifier("bus_classifier_file")
+bus_classifier = cv2.CascadeClassifier("modules/bus.xml")
 
 #* Training algo for pedestraints
-pedestrians_classifier_file = "modules/pedestrians.xml"
-pedestrians_classifier = cv2.CascadeClassifier("pedestrians_classifier_file")
+pedestrian_classifier = cv2.CascadeClassifier("modules/pedestrian.xml")
 
 font = cv2.FONT_HERSHEY_PLAIN
 
 
 #* Choose image to detect
-img = cv2.imread("data/data1.jpg")
+# data_input = cv2.imread("data/data1.jpg")
+data_input = cv2.imread("data/data1.jpeg")
 
 # # #* Convert the data to greyscale
-greyscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+greyscaled_data = cv2.cvtColor(data_input, cv2.COLOR_BGR2GRAY)
 
-# # #* Detect faces
-face_coordinates = car_classifier.detectMultiScale(greyscaled_img)
+# # #* Detect individual property
+pedestrian_identify = pedestrian_classifier.detectMultiScale(greyscaled_data)
+bike_identify = bike_classifier.detectMultiScale(greyscaled_data)
+bus_identify = bus_classifier.detectMultiScale(greyscaled_data)
+car_identify = car_classifier.detectMultiScale(greyscaled_data)
+
 
 # # #* Draw rectangles around faces
-colors = ((256,0,0), (0,256,0), (0,0,256))
 
-for (x, y, w, h) in face_coordinates:
-    color = random.choice(colors)
-    cv2.rectangle(img, (x, y),(x+w, y+h), color, 2)
-    cv2.putText(img, str("Cars"), (x+5,y-5), font,1,color,1)
+car_color = (256,125,0)
+bike_color = (0,256,0)
+bus_color = (0,0,256)
+pedestrian_color = (125,256,0)
 
-cv2.imshow("Test data1", img)
+
+for (x, y, w, h) in bus_identify:
+    cv2.rectangle(data_input, (x, y),(x+w, y+h), bus_color, 2)
+    cv2.putText(data_input, str("Bus"), (x+5,y-5), font,1,bus_color,1)
+
+for (x, y, w, h) in car_identify:
+    cv2.rectangle(data_input, (x, y),(x+w, y+h), car_color, 2)
+    cv2.putText(data_input, str("Cars"), (x+5,y-5), font,1,car_color,1)
+
+for (x, y, w, h) in bike_identify:
+    cv2.rectangle(data_input, (x, y),(x+w, y+h), bike_color, 2)
+    cv2.putText(data_input, str("Bike"), (x+5,y-5), font,1,bike_color,1)
+
+for (x, y, w, h) in pedestrian_identify:
+    cv2.rectangle(data_input, (x, y),(x+w, y+h), pedestrian_color, 2)
+    cv2.putText(data_input, str("Person"), (x+5,y-5), font,1,pedestrian_color,1)
+
+cv2.imshow("Test data1", data_input)
 
 cv2.waitKey()
+cv2.destroyAllWindows()
 
 
 # #* Capture video from webcam
@@ -89,7 +107,7 @@ cv2.waitKey()
 #     cv2.imshow("Test data", frame)
 #     key = cv2.waitKey(1)
 
-#     if key == 81 or key == 113:
+#     if key == 27:
 #         show = False
 
 # webcam.release()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+for (x, y, w, h) in car_identify:
+#     cv2.rectangle(data_input, (x, y),(x+w, y+h), car_color, 2)
+#     cv2.putText(data_input, str("Cars"), (x+5,y-5), font,1,car_color,1)
